07b1af9cabbfa0c4b0f9db738bf775f26d8cec88
EnsDevelop <chemins60217@gmail.com>
2024-03-10 01:10:22 +0900

    feat: like algorithm

--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -13,15 +13,34 @@ export class LikeService {
     private userCampingEntity: Repository<UserCampingEntity>,
     @InjectRepository(AuthEntity) private authEntity: Repository<AuthEntity>,
     @InjectRepository(CampingEntity)
-    private campinEntity: Repository<CampingEntity>,
+    private campingEntity: Repository<CampingEntity>,
     private authService: AuthService,
   ) {
     this.userCampingEntity = userCampingEntity;
     this.authService = authService;
-    this.campinEntity = campinEntity;
+    this.campingEntity = campingEntity;
   }
 
-  async like(token, campingID) {
+  async like(token: string, campingID: number) {
+    const { email } = await this.authService.validateAccess(token);
+    const user = await this.authEntity.findOneBy({ email });
+    const camping = await this.campingEntity.findOneBy({ campingID });
 
+    const existingLike = await this.userCampingEntity.findOne({
+      where: { camping, user },
+    });
+
+    if (existingLike) {
+      await this.userCampingEntity.update(existingLike, {
+        is_Valid: !existingLike.is_Valid,
+      });
+    } else {
+      await this.userCampingEntity.save({
+        camping,
+        user,
+      });
+    }
+
+    return existingLike;
   }
-} 
+}
