1ac9c5508e3995ec5654a53f31473694b3d7a237
EnsDevelop <chemins60217@gmail.com>
2024-03-14 23:08:20 +0900

    feat: camping token validate setting

--- src/camping/camping.module.ts
+++ src/camping/camping.module.ts
@@ -3,10 +3,35 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CampingEntity } from './entity/camping.entity';
 import { CampingService } from './camping.service';
 import { CampingController } from './camping.controller';
+import { AuthEntity } from 'src/auth/entity/auth.entity';
+import { UserCampingLikesEntity } from 'src/like/entity/like.entity';
+import { UserCampingReviewsEntity } from 'src/review/entity/review.entity';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CampingEntity])],
-  providers: [CampingService],
+  imports: [
+    TypeOrmModule.forFeature([
+      CampingEntity,
+      AuthEntity,
+      UserCampingLikesEntity,
+      UserCampingReviewsEntity,
+    ]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('SECRETORPRIVATE'),
+        signOptions: {
+          expiresIn: '4h',
+        },
+        verifyOptions: {
+          complete: false,
+        },
+      }),
+    }),
+  ],
+  providers: [CampingService, AuthService],
   controllers: [CampingController],
 })
 export class CampingModule {}
