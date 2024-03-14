84d2630a28feb45b0fc1f0eefa2c8f7323b6333a
EnsDevelop <chemins60217@gmail.com>
2024-03-14 23:08:50 +0900

    feat: get camping detail

--- src/camping/camping.service.ts
+++ src/camping/camping.service.ts
@@ -2,44 +2,58 @@ import { Injectable } from '@nestjs/common';
 import { CampingEntity } from './entity/camping.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthEntity } from 'src/auth/entity/auth.entity';
+import { UserCampingLikesEntity } from 'src/like/entity/like.entity';
+import { UserCampingReviewsEntity } from 'src/review/entity/review.entity';
 
 @Injectable()
 export class CampingService {
   constructor(
     @InjectRepository(CampingEntity)
     private campingEntity: Repository<CampingEntity>,
+    @InjectRepository(AuthEntity) private authEntity: Repository<AuthEntity>,
+    @InjectRepository(UserCampingLikesEntity)
+    private userCampingLikesEntity: Repository<UserCampingLikesEntity>,
+    @InjectRepository(UserCampingReviewsEntity)
+    private userCampingReviewsEntity: Repository<UserCampingReviewsEntity>,
+    private authService: AuthService,
   ) {
     this.campingEntity = campingEntity;
+    this.authEntity = authEntity;
   }
 
-  async createCamping(camping) {
-    // const { email } = await this.authService.validateAccess(token);
+  async createCamping(token, camping) {
+    const { email } = await this.authService.validateAccess(token);
+    if (email) {
+      const {
+        placeName,
+        country,
+        address,
+        number,
+        period,
+        homepage,
+        content,
+        facility,
+      } = camping;
 
-    const {
-      placeName,
-      country,
-      address,
-      number,
-      period,
-      homepage,
-      content,
-      facility,
-    } = camping;
+      const thisCamping = await this.campingEntity.findOneBy({
+        placeName,
+      });
 
-    const thisCamping = await this.campingEntity.findOneBy({
-      placeName,
-    });
-
-    await this.campingEntity.save({
-      placeName,
-      country,
-      address,
-      number,
-      period,
-      homepage,
-      content,
-      facility,
-    });
+      if (thisCamping) {
+        await this.campingEntity.save({
+          placeName,
+          country,
+          address,
+          number,
+          period,
+          homepage,
+          content,
+          facility,
+        });
+      }
+    }
   }
 
   async paginate(page = 1): Promise<any> {
@@ -58,5 +72,31 @@ export class CampingService {
         last_page: Math.ceil(total / take),
       },
     };
+  }
+
+  async getAbout(token: string, campingID: string) {
+    const camping = await this.campingEntity.findOneBy({
+      campingID: parseInt(campingID),
+    });
+    if (camping) {
+      const existingReviews = await this.userCampingReviewsEntity.find({
+        where: { camping },
+        relations: ['user'],
+      });
+      camping.reviews = existingReviews;
+      if (token) {
+        const { email } = await this.authService.validateAccess(token);
+        const user = await this.authEntity.findOneBy({ email });
+        const existingLike = await this.userCampingLikesEntity.findOne({
+          where: { camping, user },
+        });
+
+        camping.like = existingLike.is_Valid;
+
+        return camping;
+      } else {
+        camping.like = false;
+      }
+    }
   }
 }
